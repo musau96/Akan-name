5ed6bf4ed920ba1a2fa6fffcf3b8bd8d844ef3a4
Musau Nzioka <musaunzioka56@gmail.com>
2020-02-17 07:26:09 +0300

    Added a form and js function

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function submit(){
+    var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sartuday"];
+    var maleAkanNames = ["Kwasi","Kwado","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var femaleAkannames =["Akosua","Kwado","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
+}
